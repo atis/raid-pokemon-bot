ca96893ff44a652c9823857046052e12e673f709
atis <githubcom@atis.id.lv>
2017-08-08 04:18:58 +0300

    Update arrival time

--- modules/vote_arrived.php
+++ modules/vote_arrived.php
@@ -9,9 +9,9 @@
 			$rs = my_query($query);
 			$row = $rs->fetch_assoc();
 
-			my_query('INSERT INTO attendance SET raid_id='.$data['id'].', user_id='.$update['callback_query']['from']['id'].', arrived=1, team="'.$row['team'].'"');
+			my_query('INSERT INTO attendance SET raid_id='.$data['id'].', user_id='.$update['callback_query']['from']['id'].', arrived=1, team="'.$row['team'].'", attend_time=NOW()');
 		} else {
-			my_query('UPDATE attendance SET arrived=1,raid_done=0,cancel=0 WHERE raid_id='.$data['id'].' AND user_id='.$update['callback_query']['from']['id'].'');
+			my_query('UPDATE attendance SET arrived=1,raid_done=0,cancel=0,attend_time=NOW() WHERE raid_id='.$data['id'].' AND user_id='.$update['callback_query']['from']['id'].'');
 		}
 		
 		send_response_vote($update, $data);
