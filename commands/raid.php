0a658d2c1acdc0b9273be4d668052f03f80eba68
Michael <Chefkeks@users.noreply.github.com>
2017-10-20 16:44:27 +0200

    Add optional start_time in minutes (like a countdown until the raid egg will open) as 9th parameter, make SQL queries work with and without start_time in addition to first_seen
    Add a default raid duration which will be used, when time left is 0 or empty (this time can be configured in config.php, default is currently 60 minutes)
    Introduce variables for $data[x] to make code a bit better readable

--- commands/raid.php
+++ commands/raid.php
@@ -8,8 +8,8 @@ $tz = TIMEZONE;
 // Get data from message text. (remove: "/raid ")
 $gym_data = trim(substr($update['message']['text'], 5));
 
-// Create data array (max. 8)
-$data = explode(',', $gym_data, 8);
+// Create data array (max. 9)
+$data = explode(',', $gym_data, 9);
 
 /**
  * Info:
@@ -19,8 +19,9 @@ $data = explode(',', $gym_data, 8);
  * [3] = minutes
  * [4] = gym team
  * [5] = gym name
- * [6] = district
- * [7] = street
+ * [6] = district (or street)
+ * [7] = street (or district)
+ * [8] = optional: raid countdown minutes
  */
 
 // Invalid data received.
@@ -28,6 +29,9 @@ if (count($data) < 8) {
     send_message($update['message']['chat']['id'], 'Invalid input', []);
     exit;
 }
+
+// Raid boss name
+$boss = $data[0];
 
 // Get latitude / longitude from data.
 $lat = floatval($data[1]);
@@ -37,11 +41,28 @@ $lon = floatval($data[2]);
 $lat = substr($lat, 0, strpos('.', $lat) + 9);
 $lon = substr($lon, 0, strpos('.', $lon) + 9);
 
+// Endtime from input / config
+if ((!empty($data[3])) && ($data[3] > 0)) {
+    $endtime = $data[3];
+} else {
+    $endtime = RAID_DURATION;
+}
+
+// Team
+$team = $data[4];
+
 // Escape comma in Raidname
 $name = str_replace('|',',',$data[5]);
 
 // Build address string.
+// Based on input order of [6] and [7] it'll be either: Street, District or District, Street
 $address = (!empty($data[6]) ? $data[6] : '') . (!empty($data[7]) ? ", " . $data[7] : "");
+
+// Get countdown minutes when specified, otherwise 0 minutes until raid starts
+$countdown = 0;
+if (!empty($data[8])) {
+    $countdown = $data[8];
+}
 
 // Address found.
 if (!empty($address)) {
@@ -49,13 +70,14 @@ if (!empty($address)) {
     $rs = my_query(
         "
         INSERT INTO   raids
-        SET           pokemon = '{$db->real_escape_string($data[0])}',
+        SET           pokemon = '{$db->real_escape_string($boss)}',
 		              user_id = {$update['message']['from']['id']},
 		              lat = '{$lat}',
 		              lon = '{$lon}',
 		              first_seen = NOW(),
-		              end_time = DATE_ADD(first_seen, INTERVAL {$data[3]} MINUTE),
-		              gym_team = '{$db->real_escape_string($data[4])}',
+		              start_time = DATE_ADD(first_seen, INTERVAL {$countdown} MINUTE),
+		              end_time = DATE_ADD(start_time, INTERVAL {$endtime} MINUTE),
+		              gym_team = '{$db->real_escape_string($team)}',
 		              gym_name = '{$db->real_escape_string($name)}',
 		              timezone = '{$tz}',
 		              address = '{$db->real_escape_string($address)}'
@@ -68,14 +90,15 @@ if (!empty($address)) {
     $rs = my_query(
         "
         INSERT INTO   raids
-        SET           pokemon = '{$db->real_escape_string($data[0])}',
+        SET           pokemon = '{$db->real_escape_string($boss)}',
 		              user_id = {$update['message']['from']['id']},
 		              lat = '{$lat}',
 		              lon = '{$lon}',
 		              first_seen = NOW(),
-		              end_time = DATE_ADD(first_seen, INTERVAL {$data[3]} MINUTE),
-		              gym_team = '{$db->real_escape_string($data[4])}',
-		              gym_name = '{$db->real_escape_string($data[5])}',
+		              start_time = DATE_ADD(first_seen, INTERVAL {$countdown} MINUTE),
+		              end_time = DATE_ADD(start_time, INTERVAL {$endtime} MINUTE),
+		              gym_team = '{$db->real_escape_string($team)}',
+		              gym_name = '{$db->real_escape_string($name)}',
 		              timezone = '{$tz}'
         "
     );
@@ -92,6 +115,7 @@ $rs = my_query(
     "
     SELECT    *,
 		      UNIX_TIMESTAMP(end_time)                        AS ts_end,
+		      UNIX_TIMESTAMP(start_time)                      AS ts_start,
 		      UNIX_TIMESTAMP(NOW())                           AS ts_now,
 		      UNIX_TIMESTAMP(end_time)-UNIX_TIMESTAMP(NOW())  AS t_left
 	FROM      raids
