2f07863f2bdef0831f02c2cf8d363f626ac66267
Michael <Chefkeks@users.noreply.github.com>
2017-10-20 16:35:51 +0200

    Add support for the /new command to the raid_create module
    Also add start_time to SQL queries, introduce some variables instead of always using e.g. $update['message']['from']['id'] and make fullAddress a bit better readable

--- modules/raid_create.php
+++ modules/raid_create.php
@@ -1,15 +1,69 @@
 <?php
 $tz = TIMEZONE;
 
-// Get latitude / longitude values.
-$lat = $update['message']['location']['latitude'];
-$lon = $update['message']['location']['longitude'];
+// Get latitude / longitude values from Telegram Mobile Client
+if (isset($update['message']['location']['latitude'])) {
+    $lat = $update['message']['location']['latitude'];
+}
+if (isset($update['message']['location']['longitude'])) {
+    $lon = $update['message']['location']['longitude'];
+}
+
+// Get Userid and chatid from message or set below in case we get latitude and longitude by text message
+if (isset($update['message']['from']['id'])) {
+    $userid = $update['message']['from']['id'];
+}
+if (isset($update['message']['chat']['id'])) {
+    $chatid = $update['message']['chat']['id'];
+}
+if (isset($update['message']['chat']['type'])) {
+    $chattype = $update['message']['chat']['type'];
+}
+
+// Get latitude / longitude from message text if empty
+// Necessary for Telegram Desktop Client as you cannot send a location :(
+if (empty($lat) && empty($lon)) {
+    // Get the userid, chat id and type
+    $id_type = $data['id'];
+
+    // Create data array (max. 2)
+    $userdata = explode(',', $id_type, 2);
+
+    // Set userid, chat id and type
+    $userid = $userdata[0];
+    $chatid = $userid;
+    $chattype = $userdata[1];
+
+    // Debug
+    debug_log('User ID=' . $userid);
+    debug_log('Chat type=' . $chatid);
+    debug_log('Chat type=' . $chattype);
+
+    // Get lat and lon from message text
+    $coords = $data['arg'];
+
+    // Create data array (max. 2)
+    $data = explode(',', $coords, 2);
+
+    // Set latitude / longitude
+    $lat = $data[0];
+    $lon = $data[1];
+
+    // Debug
+    debug_log('Lat=' . $lat);
+    debug_log('Lon=' . $lon);
+}
 
 // Get the address.
 $addr = get_address($lat, $lon);
 
-// Get full address.
-$fullAddress = (!empty($addr['district']) ? $addr['district'] : '') . (!empty($addr['street']) ? ", " . $addr['street'] : "");
+// Get full address - Street #, ZIP District
+$fullAddress = "";
+$fullAddress .= (!empty($addr['street']) ? $addr['street'] : "");
+$fullAddress .= (!empty($addr['street_number']) ? " " . $addr['street_number'] : "");
+$fullAddress .= ", ";
+$fullAddress .= (!empty($addr['postal_code']) ? $addr['postal_code'] . " " : "");
+$fullAddress .= (!empty($addr['district']) ? $addr['district'] : "");
 
 // Address found.
 if (!empty($addr)) {
@@ -17,10 +71,11 @@ if (!empty($addr)) {
     $rs = my_query(
         "
         INSERT INTO   raids
-        SET           user_id = {$update['message']['from']['id']},
+        SET           user_id = {$userid},
 			          lat = '{$lat}',
 			          lon = '{$lon}',
 			          first_seen = NOW(),
+			          start_time = NOW(),
 			          timezone = '{$tz}',
 			          address = '{$db->real_escape_string($fullAddress)}'
         "
@@ -32,10 +87,11 @@ if (!empty($addr)) {
     $rs = my_query(
         "
         INSERT INTO   raids
-        SET           user_id = {$update['message']['from']['id']},
+        SET           user_id = {$userid},
 			          lat = '{$lat}',
 			          lon = '{$lon}',
 			          first_seen = NOW(),
+			          start_time = NOW(),
 			          timezone = '{$tz}'
         "
     );
@@ -54,18 +110,21 @@ $keys = raid_edit_start_keys($id);
 $msg = 'Erstelle Raid in: <i>' . $fullAddress . '</i>';
 
 // Private chat type.
-if ($update['message']['chat']['type'] == 'private') {
+if ($chattype == 'private') {
     // Send the message.
-    send_message($update['message']['chat']['id'], $msg . CR . 'Bitte Raid level auswählen:', $keys);
+    //send_message($update['message']['chat']['id'], $msg . CR . 'Bitte Raid level auswählen:', $keys);
+    send_message($chatid, $msg . CR . 'Bitte Raid level auswählen:', $keys);
 
 } else {
-    $reply_to = $update['message']['chat']['id'];
+    //$reply_to = $update['message']['chat']['id'];
+    $reply_to = $chatid;
     if ($update['message']['reply_to_message']['message_id']) {
         $reply_to = $update['message']['reply_to_message']['message_id'];
     }
 
     // Send the message.
-    send_message($update['message']['chat']['id'], $msg . CR . 'Bitte Raid level auswählen:', $keys, ['reply_to_message_id' => $reply_to, 'reply_markup' => ['selective' => true, 'one_time_keyboard' => true]]);
+    //send_message($update['message']['chat']['id'], $msg . CR . 'Bitte Raid level auswählen:', $keys, ['reply_to_message_id' => $reply_to, 'reply_markup' => ['selective' => true, 'one_time_keyboard' => true]]);
+    send_message($chatid, $msg . CR . 'Bitte Raid level auswählen:', $keys, ['reply_to_message_id' => $reply_to, 'reply_markup' => ['selective' => true, 'one_time_keyboard' => true]]);
 }
 
 exit();
