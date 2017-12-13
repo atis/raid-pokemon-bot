46a0cbc30006cfa41497b24cae71c09c5be471ea
Chefkeks <Chefkeks@users.noreply.github.com>
2017-12-13 23:22:18 +0100

    Added button to switch view from minutes until raid start to clock time selection for raid start. Added an own 'Raid already running' button too

--- modules/edit_poke.php
+++ modules/edit_poke.php
@@ -1,6 +1,6 @@
 <?php
 // Write to log.
-debug_log('raid_edit_poke()');
+debug_log('edit_poke()');
 debug_log($update);
 debug_log($data);
 
@@ -10,28 +10,73 @@ raid_access_check($update, $data);
 // Set the id.
 $id = $data['id'];
 
-if (true) {
-    // Update pokemon in the raid table.
-    my_query(
-        "
-        UPDATE    raids
-        SET       pokemon = '{$data['arg']}'
-          WHERE   id = {$id}
-        "
-    );
+// Get the argument.
+$arg = $data['arg'];
+
+if (true || $arg == "minutes" || $arg == "clocktime") {
+    if ($arg != "minutes" || $arg != "clocktime") {
+        // Update pokemon in the raid table.
+        my_query(
+            "
+            UPDATE    raids
+                SET       pokemon = '{$data['arg']}'
+              WHERE   id = {$id}
+            "
+        );
+    }
 
     // Init empty keys array.
     $keys = array();
 
-    for ($i = 60; $i >= 0; $i = $i - 1) {
-        // Create the keys.
-        $keys[] = array(
-	    // Just show the time, no text - not everyone has a phone or tablet with a large screen...
-            //'text'          => 'noch ' . floor($i / 60) . ':' . str_pad($i % 60, 2, '0', STR_PAD_LEFT),
-            'text'          => floor($i / 60) . ':' . str_pad($i % 60, 2, '0', STR_PAD_LEFT),
-            'callback_data' => $id . ':edit_start:' . $i
-        );
+    if ($arg == "minutes") {
+	// Set switch view.
+	$switch_text = "Uhrzeit-Ansicht";
+	$switch_view = "clocktime";
+
+        for ($i = 1; $i <= 60; $i = $i + 1) {
+            // Create the keys.
+            $keys[] = array(
+                // Just show the time, no text - not everyone has a phone or tablet with a large screen...
+                'text'          => floor($i / 60) . ':' . str_pad($i % 60, 2, '0', STR_PAD_LEFT),
+                //'text'          => unix2tz($now_plus_i,$tz,"H:i"),
+                'callback_data' => $id . ':edit_start:' . $i
+            );
+        }
+    } else {
+	// Set switch view.
+	$switch_text = "Minuten-Ansicht";
+	$switch_view = "minutes";
+
+        // Timezone - maybe there's a more elegant solution as date_default_timezone_set?!
+        $tz = TIMEZONE;
+        date_default_timezone_set($tz);
+
+        // Now 
+        $now = time();
+
+        for ($i = 1; $i <= 60; $i = $i + 1) {
+	    $now_plus_i = $now + $i*60;
+            // Create the keys.
+            $keys[] = array(
+	        // Just show the time, no text - not everyone has a phone or tablet with a large screen...
+                //'text'          => floor($i / 60) . ':' . str_pad($i % 60, 2, '0', STR_PAD_LEFT),
+	        'text'	        => unix2tz($now_plus_i,$tz,"H:i"),
+                'callback_data' => $id . ':edit_start:' . $i
+            );
+        }
     }
+
+    // Raid already running
+    $keys[] = array(
+        'text'	        => "Raid läuft schon!",
+        'callback_data' => $id . ':edit_start:0' 
+    );
+
+    // Switch view: clocktime / minutes until start
+    $keys[] = array(
+        'text'	        => $switch_text,
+        'callback_data' => $id . ':edit_poke:' . $switch_view
+    );
 
     // Get the inline key array.
     $keys = inline_key_array($keys, 5);
@@ -58,10 +103,18 @@ if (!$keys) {
 }
 
 // Edit the message.
-edit_message($update, 'Wann <b>beginnt</b> der Raid?' . CR . CR . 'Raid läuft schon?' . CR .'Einfach 0:00 auswählen!', $keys);
+if ($arg == "minutes") {
+    edit_message($update, 'In wie viel Minuten <b>beginnt</b> der Raid?', $keys);
+} else {
+    edit_message($update, 'Wann <b>beginnt</b> der Raid?', $keys);
+}
 
 // Build callback message string.
-$callback_response = 'Pokemon gespeichert: ' . $data['arg'];
+if ($arg == "minutes" || $arg == "clocktime") {
+    $callback_response = 'Ansicht geändert!';
+} else {
+    $callback_response = 'Pokemon gespeichert: ' . $data['arg'];
+}
 
 // Answer callback.
 answerCallbackQuery($update['callback_query']['id'], $callback_response);
